3ca3250e533889fbbe77fa37470ff9f49128f8fd
Sam Thomas <sgthomas@gmail.com>
2016-05-20 10:58:10 -0700

    almost working

--- quiz.js
+++ quiz.js
@@ -1,9 +1,35 @@
 
 $(document).ready(function() {
-  $(".item div.answer").click(function() {
-    var classList = $(this).attr('class').split(/\s+/);
-    if ($.inArray("answer", classList) != -1) {
-      console.log ("correct");
+  var answers = new Array();
+
+  // click listener
+  $(".item a").click(function() {
+
+    // mark choice
+    $(this).parent().addClass("choice");
+
+    // check if thing clicked has answer class
+    if ($(this).hasClass("answer")) {
+      console.log("correct");
+      $(this).next().addClass("correct");
+    } else {
+      console.log("incorrect");
+      $(this).next().addClass("incorrect");
     }
+
+    // figure out what option was clicked
+    var children = $(this).parent().parent().children();
+    var i = 0;
+    $.each(children, function(i, child) {
+      // console.log($(child));
+      if ($(child).hasClass("choice")) {
+        answers.push(i)
+        return;
+      }
+    });
+
+    console.log(answers);
+
   });
+
 });
