ed4b41c74f73a0f8faaeb3766fc9b874deb4d47e
Gabriel Bridges <gabrielb71@gmail.com>
2016-05-26 14:06:14 -0700

    sixty-nine

--- quiz.js
+++ quiz.js
@@ -47,12 +47,7 @@ $(document).ready(function() {
 function addAnswer(index, array) {
   array.push(index);
 
-  if (array.length == 2) {
-    // console.log("uh-oh");
-    // if (areArraysEqual(array, [1,2])) {
-    //   console.log("your great!");
-    // }
-    if (array.length >= 2) {
+    if (array.length >= 19) {
       console.log("halleluah");
       $("#no-ending").css("visibility", "hidden");
       if (correct >= 14) {
@@ -63,35 +58,4 @@ function addAnswer(index, array) {
         $("#ending3").css("visibility", "visible");
      }
     }
-  }
-}
-
-function areArraysEqual(array1, array2) {
-   var temp = new Array();
-   if ( (!array1[0]) || (!array2[0]) ) { // If either is not an array
-      return false;
-   }
-   if (array1.length != array2.length) {
-      return false;
-   }
-   // Put all the elements from array1 into a "tagged" array
-   for (var i=0; i<array1.length; i++) {
-      key = (typeof array1[i]) + "~" + array1[i];
-   // Use "typeof" so a number 1 isn't equal to a string "1".
-      if (temp[key]) { temp[key]++; } else { temp[key] = 1; }
-   // temp[key] = # of occurrences of the value (so an element could appear multiple times)
-   }
-   // Go through array2 - if same tag missing in "tagged" array, not equal
-   for (var i=0; i<array2.length; i++) {
-      key = (typeof array2[i]) + "~" + array2[i];
-      if (temp[key]) {
-         if (temp[key] == 0) { return false; } else { temp[key]--; }
-      // Subtract to keep track of # of appearances in array2
-      } else { // Key didn't appear in array1, arrays are not equal.
-         return false;
-      }
-   }
-   // If we get to this point, then every generated key in array1 showed up the exact same
-   // number of times in array2, so the arrays are equal.
-   return true;
 }
