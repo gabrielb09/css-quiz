d44d4113288cd27a72ef0078d5bbb53ad8a9dade
Gabriel Bridges <gabrielb71@gmail.com>
2016-05-26 20:06:58 -0700

    m

--- quiz.js
+++ quiz.js
@@ -1,7 +1,7 @@
 var correct = 0;
+var total = 0;
 
 $(document).ready(function() {
-  var answers = new Array();
 
   // hide all the endings
   $.each($(".ending"), function(index, item) {
@@ -18,44 +18,50 @@ $(document).ready(function() {
 
     // check if thing clicked has answer class
     if ($(this).hasClass("answer")) {
-      console.log("correct");
+      // console.log("correct");
       $(this).next().addClass("correct");
     } else {
-      console.log("incorrect");
+      // console.log("incorrect");
       $(this).next().addClass("incorrect");
+
+      $.each($(this).parent().parent().children().children("a"), function(i, sibling) {
+        if ($(sibling).hasClass("answer")) {
+          $(sibling).next().addClass("correct");
+        }
+      });
     }
 
     // figure out what option was clicked
     var children = $(this).parent().parent().children();
-    var i = 0;
+
+    total ++;
     $.each(children, function(i, child) {
-      // console.log($(child));
-      if ($(child).hasClass("choice")) {
-        // answers.push(i)
-        addAnswer(i, answers);
-        correct ++;
+      $(child).addClass("old");
+      if ($(child).hasClass("choice") && $(child).children("a").hasClass("answer")) {
+        addCorrect();
         return;
       }
     });
 
-    console.log(answers);
-    console.log(correct);
+    console.log("Total: " + total);
+    console.log("Correct: " + correct);
 
   });
 });
 
-function addAnswer(index, array) {
-  array.push(index);
+function addCorrect() {
+  correct ++;
+  console.log("hi");
 
-    if (array.length >= 19) {
-      console.log("halleluah");
-      $("#no-ending").css("visibility", "hidden");
-      if (correct >= 14) {
-        $("#ending1").css("visibility", "visible");
-      } else if (correct >= 8 && correct <14) {
-        $("#ending2").css("visibility", "visible");
-      } else if (correct < 8) {
-        $("#ending3").css("visibility", "visible");
-     }
+  if (total >= 19) {
+    console.log("halleluah");
+    $("#no-ending").css("visibility", "hidden");
+    if (correct >= 14) {
+      $("#ending1").css("visibility", "visible");
+    } else if (correct >= 8 && correct <14) {
+      $("#ending2").css("visibility", "visible");
+    } else if (correct < 8) {
+      $("#ending3").css("visibility", "visible");
     }
+  }
 }
